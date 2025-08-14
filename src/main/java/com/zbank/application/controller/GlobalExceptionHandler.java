22c65018f46bf43801e41b7c99ffafb6c5e618c2
zed-amb <zel.ambisa@gmail.com>
2025-08-14 00:26:18 -0400

    confirm

--- src/main/java/com/zbank/application/controller/GlobalExceptionHandler.java
+++ src/main/java/com/zbank/application/controller/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+//This is global exeception
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
